b07abed5e00b08c9d82ad2bf0f0e5e2d18757f62
Arun Kumar Pandey <arunp77@gmail.com>
2024-04-04 17:37:03 +0200

    fastapi

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
 from fastapi import FastAPI
-from fastapi.params import Body
-
+users_db = [
+    {
+        'user_id': 1,
+        'name': 'Alice',
+        'subscription': 'free tier'
+    },
+    {
+        'user_id': 2,
+        'name': 'Bob',
+        'subscription': 'premium tier'
+    },
+    {
+        'user_id': 3,
+        'name': 'Clementine',
+        'subscription': 'free tier'
+    }
+]
 app = FastAPI()
+@app.get('/', description="Welcome")
+def welcome():
+    return {
+        'greetings': 'welcome'
+    }
+@app.get('/users')
+def get_users():
+    return users_db
 
-# request Get method url: "/"
-@app.get("/")
-def root():
-    return {"message" : "Welcome to my api"}
+@app.get('/users/{userid:int}')
+def get_user(userid):
+    try:
+        user = list(filter(lambda x: x.get('user_id') == userid, users_db))[0]
+        return user
+    except IndexError:
+        return {}
 
-# get post data
-@app.get("/posts")
-def get_posts():
-    return {"data" : "This is your posts"}
+@app.get('/users/{userid:int}/name')
+def get_user_name(userid):
+    try:
+        user = list(filter(lambda x: x.get('user_id') == userid, users_db))[0]
+        return {'name': user['name']}
+    except IndexError:
+        return {}
 
-@app.post("/createposts")
-def create_posts(payload : dict = Body(...)):
-    print(payload)
-    return {"new_post" : f"title {payload['title']} content : {payload['content']}"}
+@app.get('/users/{userid:int}/subscription')
+def get_user_suscription(userid):
+    try:
+        user = list(filter(lambda x: x.get('user_id') == userid, users_db))[0]
+        return {'subscription': user['subscription']}
+    except IndexError:
+        return {}
