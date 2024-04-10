6d6d672f941488ddc10169d008aca63d80b0d1e4
Arun Kumar Pandey <arunp77@gmail.com>
2024-04-10 16:33:46 +0200

    updates

--- FastApi-MCQ-Generator/api/__init__.py
+++
@@ -1,6 +0,0 @@
-# api/__init__.py
-
-# Import modules to make them available when the package is imported
-from api.endpoints import router as api_router
-from api.auth import authenticate_user
-from api.models import Question, QuestionRequest, Token, QuestionCreate
