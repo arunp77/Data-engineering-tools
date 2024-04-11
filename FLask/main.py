bc3e8a8564ce22e38a34869a553522272cdde049
Arun Kumar Pandey <arunp77@gmail.com>
2024-04-12 01:23:37 +0200

    Flask bog

--- FLask/main.py
+++ FLask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from dotenv import load_dotenv
 import os
 
@@ -33,9 +33,8 @@ def about():
 
 
 
-
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
