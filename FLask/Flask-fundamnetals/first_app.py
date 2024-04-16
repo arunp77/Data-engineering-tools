f930f1d69d6178f478eea2eed23317e8b8fb7e80
Arun Kumar Pandey <arunp77@gmail.com>
2024-04-16 16:54:01 +0200

    Flask update

--- FLask/Flask-fundamnetals/first_app.py
+++ FLask/Flask-fundamnetals/first_app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 from pydantic import BaseModel
 from flask_pydantic import validate
+from werkzeug.exceptions import NotFound
 
 class Query(BaseModel):
     name:str
     age:int
+    
+class Player(BaseModel):
+    name:str
+    position:str
+    team:str
+    birth_year:int
 
 app = Flask(__name__)
 
@@ -13,6 +20,32 @@ app = Flask(__name__)
 def intro(query:Query):
     return f"Hello, my name is {query.name} and I am {query.age} years old"
 
+@app.route("/info")
+@validate()
+def info(query:Player):
+    return query.dict()
+
+@app.route("/info_post",methods=["POST"])
+@validate()
+def info_post(body:Player):
+    return body.dict()
+
+@app.route("/head")
+def head():
+    if request.headers.get("username"):
+        return "Hello {}".format(request.headers.get("username"))
+    elif request.headers.get("enterprise"):
+        return "Hello {}".format(request.headers.get("enterprise"))
+    else:
+        return "Hello World !"
+
+@app.route("/hello")
+def hello():
+  return "Hello World"
+
+@app.errorhandler(NotFound)
+def handler_error404(err):
+  return "You have encountered an error of 404",404
 
 
 if __name__ == "__main__":
