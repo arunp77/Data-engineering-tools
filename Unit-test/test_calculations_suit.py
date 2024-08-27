50e86bcf36ccbfef36c811120f7051da558503ef
Arun Kumar Pandey <arunp77@gmail.com>
2024-08-27 20:22:19 +0200

    load_tests() Function

--- Unit-test/test_calculations_suit.py
+++ Unit-test/test_calculations_suit.py
@@ -48,6 +48,7 @@ class TestStatisticalOperations(unittest.TestCase):
     def test_mode_multiple(self):
         self.assertEqual(set(mode([1, 1, 2, 3, 4, 4, 5, 5])), {1, 4, 5})
 
+#============================================================
 """ 
 # First method
 In the make_suite() function, you create a list of all tests from the TestArithmeticOperations test case. 
@@ -55,7 +56,7 @@ In the make_suite() function, you create a list of all tests from the TestArithm
 Then, you create and return a test suite using the TestSuite() constructor with the list of tests as an argument.
 """
 
-
+## Uncomment this one and comment the both the two below to run this one using python test_calculations_suit.py
 # def make_suite():
 #     arithmetic_tests = [
 #         TestArithmeticOperations("test_add"),
@@ -70,13 +71,14 @@ Then, you create and return a test suite using the TestSuite() constructor with 
 #     runner = unittest.TextTestRunner(verbosity=2)
 #     runner.run(suite)
 
+#============================================================
 """ 
 # Second method
 This new version of make_suite() works like the previous section. Instead of using the class constructor to 
 build the test suite, you use the .addTest() method. This approach can be useful when you have an existing test 
 suite, and you need to add more tests to it.
 """
-
+## Uncomment this one and comment the above one and below one tests to run this one using python test_calculations_suit.py
 # def make_suite():
 #     arithmetic_suite = unittest.TestSuite()
 #     arithmetic_suite.addTest(TestArithmeticOperations("test_add"))
@@ -89,30 +91,76 @@ suite, and you need to add more tests to it.
 #     suite = make_suite()
 #     runner = unittest.TextTestRunner(verbosity=2)
 #     runner.run(suite)
-    
-    
+#============================================================   
+
+#============================================================
 """ 
 # Third method
 The TestSuite class also has a .addTests() method that you can use to add several tests in one go. 
 This method takes an iterable of test cases, test suites, or a combination of them. Consider the 
 following example that creates a test suite with the statistical tests:
 """
+## uncomment for this suit and comment the two above
+# def make_suite():
+#     statistical_tests = [
+#         TestStatisticalOperations("test_mean"),
+#         TestStatisticalOperations("test_median_odd"),
+#         TestStatisticalOperations("test_median_even"),
+#         TestStatisticalOperations("test_median_unsorted"),
+#         TestStatisticalOperations("test_mode_single"),
+#         TestStatisticalOperations("test_mode_multiple"),
+#     ]
+#     statistical_suite = unittest.TestSuite()
+#     statistical_suite.addTests(statistical_tests)
 
-def make_suite():
-    statistical_tests = [
-        TestStatisticalOperations("test_mean"),
-        TestStatisticalOperations("test_median_odd"),
-        TestStatisticalOperations("test_median_even"),
-        TestStatisticalOperations("test_median_unsorted"),
-        TestStatisticalOperations("test_mode_single"),
-        TestStatisticalOperations("test_mode_multiple"),
-    ]
-    statistical_suite = unittest.TestSuite()
-    statistical_suite.addTests(statistical_tests)
+#     return statistical_suite
 
-    return statistical_suite
+# if __name__ == "__main__":
+#     suite = make_suite()
+#     runner = unittest.TextTestRunner(verbosity=2)
+#     runner.run(suite)
+    
+##==================================================================================================
+##====================combined suit ========================================
+# uncomment this one if you want to test the combined unittest for the arthemtic and statsitical tests
+# def make_suite():
+#     # Create a suite for arithmetic tests
+#     arithmetic_tests = [
+#         TestArithmeticOperations("test_add"),
+#         TestArithmeticOperations("test_subtract"),
+#         TestArithmeticOperations("test_multiply"),
+#         TestArithmeticOperations("test_divide"),
+#     ]
+
+#     # Create a suite for statistical tests
+#     statistical_tests = [
+#         TestStatisticalOperations("test_mean"),
+#         TestStatisticalOperations("test_median_odd"),
+#         TestStatisticalOperations("test_median_even"),
+#         TestStatisticalOperations("test_median_unsorted"),
+#         TestStatisticalOperations("test_mode_single"),
+#         TestStatisticalOperations("test_mode_multiple"),
+#     ]
+
+#     # Combine both suites into one
+#     combined_suite = unittest.TestSuite()
+#     combined_suite.addTests(arithmetic_tests)
+#     combined_suite.addTests(statistical_tests)
+
+#     return combined_suite
+
+# if __name__ == "__main__":
+#     suite = make_suite()
+#     runner = unittest.TextTestRunner(verbosity=2)
+#     runner.run(suite)
+
+##========================Creating Suites With the load_tests() Function ======================================
+## uncomment it if you want to run the test using the load_tests
+def load_tests(loader, standard_tests, pattern):
+    suite = unittest.TestSuite()
+    suite.addTests(loader.loadTestsFromTestCase(TestArithmeticOperations))
+    suite.addTests(loader.loadTestsFromTestCase(TestStatisticalOperations))
+    return suite
 
 if __name__ == "__main__":
-    suite = make_suite()
-    runner = unittest.TextTestRunner(verbosity=2)
-    runner.run(suite)
+    unittest.main()
