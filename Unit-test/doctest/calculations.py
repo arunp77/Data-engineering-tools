6508b60aab3df7298e487646111a1319f3bb1d8d
Arun Kumar Pandey <arunp77@gmail.com>
2024-08-28 08:35:28 +0200

    Doctest and unitest separate folders added and files reorganized

---
+++ Unit-test/doctest/calculations.py
@@ -0,0 +1,12 @@
+# calculations.py
+
+def add(a, b):
+    """Compute and return the sum of two numbers.
+
+    Usage examples:
+    >>> add(4.0, 2.0)
+    6.0
+    >>> add(4, 2)
+    6.0
+    """
+    return float(a + b)
